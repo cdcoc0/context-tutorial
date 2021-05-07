ad20373d5c4ba6bfab81e73f77b88213caf234e4
velykitty <jiyunee13@gmail.com>
2021-05-07 18:19:12 +0900

    ContextAPI -color selecting functions

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ColorConsumer} from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
@@ -6,18 +7,27 @@ const SelectColors = () => {
     return (
         <div>
             <h2>Select one color🎨</h2>
-            <div style={{display: 'flex'}}>
-                {colors.map(color => (
-                    <div key={color}
-                        style={{
-                            background: color,
-                            width: '24px',
-                            height: '24px',
-                            cursor: 'pointer'
-                        }} 
-                    />
-                ))}
-            </div>
+            <ColorConsumer>
+                {({actions}) =>(
+                    <div style={{display: 'flex'}}>
+                        {colors.map(color => (
+                            <div key={color}
+                                style={{
+                                    background: color,
+                                    width: '24px',
+                                    height: '24px',
+                                    cursor: 'pointer'
+                                }}
+                                onClick={() => actions.setColor(color)}
+                                onContextMenu={e => {
+                                    e.preventDefault(); //마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 방지
+                                    actions.setSubcolor(color);
+                                }}
+                            />
+                        ))}
+                    </div>
+                )}
+            </ColorConsumer>
             <hr/>
         </div>
     );
